1fde374f4e2d5541d6564f58d75034d8c4723d91
Kurt Lee <breath103@gmail.com>
2021-09-04 18:39:01 +0900

    Initial Commit

---
+++ Flocking/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Flocking
+//
+//  Created by Kurt Lee on 9/4/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
